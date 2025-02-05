7db4bbe388a9ff9328d20c0a569e5942daf310ae
VGoku <neojohn2312@gmail.com>
2025-02-04 21:23:28 -0600

    firstPush

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].mjs",
+        chunkFileNames: "assets/[name]-[hash].mjs",
+        assetFileNames: "assets/[name]-[hash].[ext]"
+      }
+    }
+  },
   server: {
     port: 3000,
   },
